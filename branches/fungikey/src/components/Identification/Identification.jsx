713f21b6acfda246b5e641aac65dbe554abfc6ec
Assem92 <as.aoussar@gmail.com>
2022-10-25 15:40:33 +0200

    Ajout de la sidebar

--- branches/fungikey/src/components/Identification/Identification.jsx
+++ branches/fungikey/src/components/Identification/Identification.jsx
@@ -1,29 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
 
-import { Image } from "react-bootstrap";
+import React from 'react'
+import {Link} from 'react-router-dom';
+import Sidebar from './Sidebar.jsx';
+import {Image} from 'react-bootstrap'
+
 
 /**
  * @returns Page Principale d'Identification avec des aides pour l'anatomies des champignons
  */
+
 export default function Identification() {
   return (
     <>
-      <div className="container-identification">
-        <h2 className="h2-identification">Anatomie des champignons</h2>
-        <Image
-          fluid
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Sch%C3%A9ma_champignon.jpg/440px-Sch%C3%A9ma_champignon.jpg"
-        ></Image>
-        <p>Source: Wikipedia</p>
-        <h2 className="h2-identification">Tubes ou Lames? </h2>
-        <Image fluid src="./images/aideIdenti1.webp"></Image>
-        <p>Source: toxinfo.ch</p>
+    <Sidebar/>
+    <div className='container-identification'>
+      
+      <h2 className='h2-identification'>Anatomie des champignons</h2>
+      <Image fluid src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Sch%C3%A9ma_champignon.jpg/440px-Sch%C3%A9ma_champignon.jpg'></Image>
+      <p>Source: Wikipedia</p>
+      <h2 className='h2-identification'>Tubes ou Lames? </h2>
+      <Image fluid src='./images/aideIdenti1.webp'></Image>
+      <p>Source: toxinfo.ch</p>
 
-        <Link to="/">
-          <button className="button-retour">Retour</button>
-        </Link>
-      </div>
+      <Link to="/">
+        <button className='button-retour'>Retour</button>
+      </Link>
+    </div>
     </>
-  );
+  )
 }
+
---
+++ branches/fungikey/src/components/Identification/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Nav, Navbar, Container, Offcanvas, NavDropdown} from 'react-bootstrap'
+
+export default function Sidebar() {
+  return (
+    <Navbar className="color-nav"  expand={false}>
+      <Container fluid>
+      <Navbar.Brand href="/Identification">
+      <img
+          alt=""
+          src="/images/icons/60.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+        <span className="sidebar-title">Identification</span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas
+        Image
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+        >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title id="offcanvasNavbarLabel">Identification</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Nav className="justify-content-end flex-grow-1 pe-3">
+        <Nav.Link href="/IdentificatorMain">Identificator</Nav.Link>
+    
+        </Nav>
+        
+      </Offcanvas.Body>
+    </Navbar.Offcanvas>
+  </Container>
+</Navbar>
+  )
+}
