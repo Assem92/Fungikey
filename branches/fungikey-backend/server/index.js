368f37d9b69e2f376d525058134aad367620fb4d
ofachati <42001547@parisnanterre.fr>
2022-12-06 21:14:10 +0100

    backend correction

--- branches/fungikey-backend/server/index.js
+++ branches/fungikey-backend/server/index.js
@@ -1,11 +1,7 @@
-
-
-
 let ChampiJson = require("./assets/champi.json")
 let FamilleChampiJson = require("./assets/familleChampi.json")
 let FamilleComplementairesJson = require("./assets/familleComplementaires.json")
 let recette = require("./assets/recette.json")
-
 
 const express = require("express");
 
@@ -24,9 +20,8 @@ app.get("/api/champi", (req, res) => {
 
 app.get("/api/champi/:id", (req, res) => {
     const champ=ChampiJson.find(c=> c.id === parseInt(req.params.id));
-    if (!champ) res.status(404).send("le chapignion n'existe pas");
+    if (!champ) res.status(404).send("le champignion n'existe pas");
     res.json(champ);});
-
   
 app.get("/api/familleChampi", (req, res) => {
     res.json(FamilleChampiJson);});
@@ -34,23 +29,22 @@ app.get("/api/familleChampi", (req, res) => {
 app.get("/api/familleComplementaires", (req, res) => {
     res.json(FamilleComplementairesJson);});
    
-
 app.get("/api/recette", (req, res) => {
         res.json(recette);});    
 
 app.get("/api/recette/:id", (req, res) => {
         const r=recette.find(c=> c.id === parseInt(req.params.id));
-        if (!r) res.status(404).send("le chapignion n'existe pas");
+        if (!r) res.status(404).send("la recette n'existe pas");
         res.json(r);});    
 
-
 //swagger
-    app.use(
-        '/api-docs',
+app.use('/api-docs',
         swaggerUi.serve, 
         swaggerUi.setup(swaggerDocument)
       );   
 //
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
