a52e0192bc4971e03a67d18cc69a2640b3e41fc2
ofachati <42001547@parisnanterre.fr>
2023-01-29 18:23:53 +0100

    add location logo

--- branches/fungikey/src/components/Maps/GoogleMaps.jsx
+++ branches/fungikey/src/components/Maps/GoogleMaps.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+import { Navbar, Container } from "react-bootstrap";
+import "./GoogleMaps.css";
 
 const mapStyles = {
   width: "100%",
@@ -22,32 +24,52 @@ export function GoogleMaps({ google, locations = [] }) {
   }, []);
 
   return (
-    <Map
-      google={google}
-      containerStyle={{
-        position: "static",
-        width: "100%",
-        height: "100%",
-      }}
-      style={mapStyles}
-      center={userLocation}
-      initialCenter={userLocation}
-      zoom={18}
-      disableDefaultUI={true}
-    >
-      {locations.map((coords) => (
-        <Marker position={coords} />
-      ))}
-      {userLocation.lat && (
-        <Marker
-          position={userLocation}
-          icon={{
-            url: "/images/icons/userlocation.png",
-            scaledSize: new google.maps.Size(30, 30),
+    <div>
+      <Navbar className="color-nav" expand={false}>
+        <Container fluid>
+          <Navbar.Brand href="/map">
+            <img
+              alt=""
+              src="/images/icons/60.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            <span className="sidebar-title">Map</span>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <div>
+        <Map
+          className="map"
+          google={google}
+          containerStyle={{
+            position: "static",
+            width: "100%",
+            height: "100%",
           }}
-        />
-      )}
-    </Map>
+          style={mapStyles}
+          center={userLocation}
+          initialCenter={userLocation}
+          zoom={18}
+          disableDefaultUI={true}
+        >
+          {locations.map((coords) => (
+            <Marker position={coords} />
+          ))}
+          {userLocation.lat && (
+            <Marker
+              position={userLocation}
+              icon={{
+                url: "/images/icons/userlocation.png",
+                scaledSize: new google.maps.Size(30, 30),
+              }}
+            />
+          )}
+        </Map>
+      </div>
+    </div>
   );
 }
 
