ab6ddee7c61293d38ab9100e66ec0b676dc5a90b
MohamedKonate <61732244+MohamedKonate@users.noreply.github.com>
2023-03-24 00:10:14 +0100

    Update Card.jsx

--- Branches/Fungikey/src/components/JeuMemoryCards/Card.jsx
+++ Branches/Fungikey/src/components/JeuMemoryCards/Card.jsx
@@ -2,7 +2,7 @@ function Card({ item, id, handleClick }) {
   const itemClass = item.stat ? " active " + item.stat : "";
 
   return (
-    <div className={"card" + itemClass} onClick={() => handleClick(id)}>
+    <div className={"card1" + itemClass} onClick={() => handleClick(id)}>
       <img src={item.img} alt="" />
     </div>
   );
