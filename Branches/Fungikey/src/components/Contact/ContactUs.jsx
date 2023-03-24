ba932287ca5bd081a119e2e6161731f6a28eb136
ofachati <42001547@parisnanterre.fr>
2023-03-24 21:41:20 +0100

    edit contact

--- Branches/Fungikey/src/components/Contact/ContactUs.jsx
+++ Branches/Fungikey/src/components/Contact/ContactUs.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { contactConfig } from "./option";
 import CustomNavbar from "../utilities/NavBar/CustomNavbar";
+
 export default function ContactUs() {
+  const [message, setMessage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setMessage(event.target.elements.message.value);
+    event.target.reset();
+  }
+
   return (
     <div>
-      <CustomNavbar title="Contactez nous" link="/" />
-      <br />
+      {" "}
+      <CustomNavbar title="Contactez-nous" link="/" />
       <Container>
         <Row className="sec_sp">
           <Col lg="5" className="mb-5">
             <h3 className="color_sec py-5">Restez en contact</h3>
+            <div className="mt-5 center">
+              <strong>Sommaire de votre message:</strong>
+              <p>{message}</p>
+            </div>
             <address>
               <strong>Adresse mail:</strong>{" "}
               <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
@@ -29,8 +42,9 @@ export default function ContactUs() {
             </address>
             <p>{contactConfig.description}</p>
           </Col>
+
           <Col lg="7" className="d-flex align-items-center">
-            <form className="contact__form w-100">
+            <form onSubmit={handleSubmit} className="contact__form w-100">
               <Row>
                 <Col lg="6" className="form-group">
                   <input
@@ -63,8 +77,8 @@ export default function ContactUs() {
               ></textarea>
               <br />
               <Row>
-                <Col lg="6" className="form-group">
-                  <Button className="btn btn-primary" type="submit">
+                <Col lg="6" className="form-group d-flex">
+                  <Button className="btn btn-primary mr-3" type="submit">
                     Envoyer
                   </Button>
                   <Link to="/">
